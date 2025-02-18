ebd168c785efaa565762b0b72bf74a34d51da08f
Osei Rick <rickoseiampah@gmail.com>
2025-02-18 16:02:38 +0000

    little ui patch

--- app/company/[slug]/page.tsx
+++ app/company/[slug]/page.tsx
@@ -139,8 +139,13 @@ const page = async ({ params }: { params: { slug: string } }) => {
           chartData={chartData}
         />
         <div className="flex flex-col md:flex-row gap-3 sm:gap-6 lg:gap-20">
-          <div className={style["main-section"] + " basis-[60%] max-w-screen-size-size sm:max-w-[50vw] w-full"}>
-            <div className={style[""]}>
+          <div
+            className={
+              style["main-section"] +
+              " basis-[60%] max-w-screen-size-size sm:max-w-[50vw] w-full"
+            }
+          >
+            <div className="w-full">
               <RatesSection
                 companyDetails={companyDetails}
                 companyData={companyData}
@@ -160,7 +165,10 @@ const page = async ({ params }: { params: { slug: string } }) => {
           </div>
           <div className=" basis-[40%] w-full  flex flex-col gap-6 items-center">
             {companyDetails?.company?.category == "exchangeRates" && (
-              <CurrencyConverter className="sm:block hidden" companyData={companyData} />
+              <CurrencyConverter
+                className="sm:block hidden"
+                companyData={companyData}
+              />
             )}
             {/* <div className="sm:w-auto sm:px-0 px-5 w-screen overflow-x-scroll">
               <LocationsList />
